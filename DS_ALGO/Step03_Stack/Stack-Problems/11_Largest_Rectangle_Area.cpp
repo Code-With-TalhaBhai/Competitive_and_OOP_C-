0c5f82214b3b1a0edfe7d91857726cb0f2b25e63
= <=>
2024-04-05 18:09:45 +0500

    histogram program done

--- DS_ALGO/Step03_Stack/Stack-Problems/11_Largest_Rectangle_Area.cpp
+++ DS_ALGO/Step03_Stack/Stack-Problems/11_Largest_Rectangle_Area.cpp
@@ -6,6 +6,11 @@ using namespace std;
 int LargestRectangularArea1(vector<int> heights){
     int size = heights.size();
     int area,max_area = 0;
+
+    if(size == 1){
+        return heights[0];
+    }
+
     for (int i = 0; i < size-1; i++)
     {
         int width = 1;
@@ -40,51 +45,41 @@ void print_vect(vector<int> v){
 
 vector<int> prev_smaller(vector<int> arr){
     int size = arr.size();
+    vector<int> ans(size);
     stack<int> S;
     S.push(-1);
 
-    int j;
     for (int i = 0; i < size; i++)
     {
-        j = i-1; // {5,8,9,10,1,5,6} // {2,4}
         int curr = arr[i];
-        while(S.top()>curr){
+        while(S.top() != -1 && arr[S.top()] >= curr){
             S.pop();
-            j--;
         }
 
-        if(j == 0){
-            j = -1;
-        }
-        arr[i] = j;
-        S.push(curr);
-
+        ans[i] = S.top();
+        S.push(i);
     }
-    print_vect(arr);
-return arr;
+    // print_vect(ans);
+return ans;
 }
+
 
 vector<int> next_smaller(vector<int> arr){
     int size = arr.size();
+    vector<int> ans(size);
     stack<int> S;
     S.push(-1);
-    int j;
     for(int i=size-1; i>=0; i--){
-        j = i+1;
         int curr = arr[i];
-        while(S.top() > curr){
+        while(S.top() != -1 && arr[S.top()] >= curr){
             S.pop();
-            j++;
-        }
-        if(j == size-1){
-            j = size;
         }
 
-        arr[i] = j;
-        S.push(curr);
+        ans[i] = S.top();
+        S.push(i);
     }
-    print_vect(arr);
-    return arr;
+    // print_vect(ans);
+    return ans;
 }
 
 
@@ -92,10 +87,14 @@ int LargestRectangularArea2(vector<int> heights){
     
     int max_area = INT16_MIN;
     int area,width,length;
-    for (int i = 0; i < heights.size(); i++)
+    int size = heights.size();
+    for (int i = 0; i < size; i++)
     {
         int p_smaller = prev_smaller(heights)[i];
         int n_smaller = next_smaller(heights)[i];
+        if (next_smaller(heights)[i] == -1){
+            n_smaller = size;
+        }
 
         length = heights[i];
         width = n_smaller - p_smaller - 1;
@@ -114,18 +113,24 @@ int main(){
     // vector<int> heights = {2,1,5,6,2,3};
     // vector<int> heights = {6,2,5,4,1,5,6};
     // vector<int> heights = {5,8,9,10,1,5,6};
-    // vector<int> heights = {3, 5, 1, 7, 5, 9};
-    vector<int> heights = {2,4};
+    vector<int> heights = {3, 5, 1, 7, 5, 9};
+    // vector<int> heights = {2,4};
+    // vector<int> heights = {4,2};
     // vector<int> heights = {0};
+    // vector<int> heights = {1};
+    // vector<int> heights = {1,1};
+    // vector<int> heights = {0,9};
 
-    // Without Optimization (O(n*2))
+    // vector<int> heights(300,1);
+
+    // Without Optimization (O(n*2)) Not always gives correct area but mostly does
     cout << "The largest rectangular area is: " << LargestRectangularArea1(heights) << endl;
 
     // With Stack(Optimized(O(n)))
     cout << "The largest rectangular area with stack is: " << LargestRectangularArea2(heights) << endl;
 
-    prev_smaller(heights);
-    next_smaller(heights);
+    // prev_smaller(heights);
+    // next_smaller(heights);
 
     return 0;
 }
